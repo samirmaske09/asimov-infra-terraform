77c11b9203f3ea2d8c8c4d0d2af2264783b71a86
samirmaske23 <samirmaske23091998@gaol.com>
2025-09-24 19:20:32 +0530

    file added

--- file.py
+++ file.py
@@ -2,23 +2,35 @@ from constructs import Construct
 from cdktf import App, TerraformStack, TerraformOutput
 from imports.aws.provider import AwsProvider
 from imports.aws.s3_bucket import S3Bucket
+from imports.aws.vpc import Vpc
 
 
-class S3Stack(TerraformStack):
+class MyInfraStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
+        # AWS provider
         AwsProvider(self, "AWS", region="us-east-1")
 
+        # --- S3 bucket ---
         bucket = S3Bucket(
             self,
             "MyBucket",
-            bucket="circleci-cdktf-bucket-123456"
+            bucket="circleci-cdktf-bucket-12345656433"
         )
 
         TerraformOutput(self, "bucket_name", value=bucket.bucket)
 
+        # --- VPC ---
+        vpc = Vpc(
+            self,
+            "MyVpc",
+            cidr_block="10.0.0.0/16"
+        )
+
+        TerraformOutput(self, "vpc_id", value=vpc.id)
+
 
 app = App()
-S3Stack(app, "s3-stack")
+MyInfraStack(app, "infra")
 app.synth()
